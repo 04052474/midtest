083d76fa30a399867b5aca386266db8a9072a41f
04052474 <37133566+04052474@users.noreply.github.com>
2018-04-18 15:23:56 +0800

    Add files via upload

---
+++ ch1/ch1.ino
@@ -0,0 +1,24 @@
+void setup() {
+ pinMode(7,OUTPUT);
+ pinMode(6,INPUT);
+
+}
+
+void loop() {
+  boolean val=digitalRead(6);
+  if(val)
+  {
+    digitalWrite(7,HIGH);
+    delay(250);
+    digitalWrite(7,LOW);
+    delay(250);
+    }
+    else
+    {
+     digitalWrite(7,HIGH);
+    delay(500);
+      digitalWrite(7,LOW);
+    delay(500); 
+      }
+
+}
---
+++ ch3/ch3.ino
@@ -0,0 +1,31 @@
+const byte pin=A0;
+int val;
+float R=1087600;
+float Vin=5;
+float sensorVoltage, sensorValue;
+void setup() {
+  Serial.begin(9600);
+  pinMode(14,OUTPUT);
+  sensorVoltage = 0;
+   sensorValue = 0;
+}
+
+void loop() {
+  byte i;
+  float R1,R2;
+   
+  for(i=0;i<=5;i++)
+  sensorValue = 0.7*sensorValue+0.3*analogRead(A0);
+  
+  Serial.print("sensorValue=");
+  Serial.print(sensorValue);
+  sensorVoltage = sensorValue*5/4095;
+  R1=sensorVoltage*R/Vin;
+  R2 = R-R1;
+  sensorVoltage = Vin * R1 / (R1+R2);
+  Serial.print("  sensorVoltage=");
+  Serial.print(sensorVoltage);
+  Serial.println();
+  delay(500);
+
+}
